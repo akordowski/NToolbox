c8f07d59787813a3c7251534da2f4d78eab51b33
Artur Kordowski <artur.kordowski@gmail.com>
2016-02-28 18:52:25 +0100

    Add DictionaryExtension class.

---
+++ NToolbox.Tests/NToolbox/Extensions/DictionaryExtensionTest.cs
@@ -0,0 +1,37 @@
+﻿using NToolbox.Extensions;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace NToolbox.Tests.NToolbox.Extensions
+{
+	[TestFixture]
+	public class DictionaryExtensionTest
+	{
+		#region Private Properties
+		private Dictionary<string, string> _dict;
+		#endregion
+
+		#region SetUp
+		[SetUp]
+		public void SetUp()
+		{
+			_dict = new Dictionary<string, string>
+			{
+				{ "123", "abc" },
+				{ "456", "def" }
+			};
+		}
+		#endregion
+
+		#region Test Case Sources
+		[TestCase("123", "abc", true)]
+		[TestCase("123", "def", false)]
+		[TestCase("456", "def", true)]
+		[TestCase("456", "abc", false)]
+		public void Contains_Returns_Valid_Result(string key, string value, bool expectedResult)
+		{
+			Assert.That(_dict.Contains(key, value), Is.EqualTo(expectedResult));
+		}
+		#endregion
+	}
+}
---
+++ NToolbox/Extensions/DictionaryExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace NToolbox.Extensions
+{
+	/// <summary>
+	/// Contains IDictionary extension methods.
+	/// </summary>
+	public static class DictionaryExtension
+	{
+		#region Public Static Methods
+		/// <summary>
+		/// Determines whether the <see cref="IDictionary&lt;TKey, TValue&gt;"/> contains an <see cref="KeyValuePair&lt;TKey, TValue&gt;"/> with the specified key and value.
+		/// </summary>
+		/// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+		/// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+		/// <param name="dictionary"></param>
+		/// <param name="key">The key to locate in the <see cref="IDictionary&lt;TKey, TValue&gt;"/>.</param>
+		/// <param name="value">The value to locate in the <see cref="IDictionary&lt;TKey, TValue&gt;"/>. The value can be <strong>null</strong> for reference types.</param>
+		/// <returns><strong>true</strong> if the <see cref="IDictionary&lt;TKey, TValue&gt;"/> contains an <see cref="KeyValuePair&lt;TKey, TValue&gt;"/> with the specified key and value; otherwise, <strong>false</strong>.</returns>
+		public static bool Contains<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,  TKey key, TValue value)
+		{
+			return dictionary.Contains(new KeyValuePair<TKey, TValue>(key, value));
+		}
+		#endregion
+	}
+}
